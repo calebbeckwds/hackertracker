2023e60bca7cf994fc31fdd1c0f699968ab0f0e3
cbeck89 <tcmxb89@yahoo.com>
2013-02-23 18:08:29 -0500

    initial commit, rails environment created.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackertracker::Application.config.secret_token = '2e3dca48078a1ca05286c8292d43a7dce1484b6cc24c28a30cb6ebee10bcbb38cff98dd1a3c2e70ec5685e0cf8659ac37d75f662c7d4af927b85339b85cd0d62'
