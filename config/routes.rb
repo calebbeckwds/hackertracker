5ea482ab4434b03af5c81202255c5c5b8d082c4d
Daniel Lewis <daniel.lewis@toptal.com>
2013-02-23 19:06:37 -0500

    added action for landing page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Hackertracker::Application.routes.draw do
 
   resources :users
 
+  root to: 'pages#landing'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
