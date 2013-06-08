49162c60ac47f349ce0a97fd573172aeded9bbf9
Daniel Lewis <daniel.lewis@toptal.com>
2013-06-08 07:36:00 -0400

    hoops

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,5 +1,5 @@
 class Area < ActiveRecord::Base
-  attr_accessible :desc, :name, :photo, :ordering
+  attr_accessible :desc, :name, :photo, :sequence
 
   has_many :unique_items
   has_many :tickets, :through => :unique_items
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130608111846) do
+ActiveRecord::Schema.define(:version => 20130608113436) do
 
   create_table "areas", :force => true do |t|
     t.string   "name"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(:version => 20130608111846) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
-    t.integer  "ordering"
+    t.integer  "sequence"
   end
 
   create_table "caveats", :force => true do |t|
