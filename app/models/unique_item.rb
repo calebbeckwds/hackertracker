7c790e61c40e72d51c13af7af10c5571b0a98142
Daniel Lewis <daniel.lewis@toptal.com>
2013-06-08 06:30:04 -0400

    fixes stuff

--- app/models/unique_item.rb
+++ app/models/unique_item.rb
@@ -1,5 +1,5 @@
 class UniqueItem < ActiveRecord::Base
-  attr_accessible :area, :loggable, :name, :photo, :ticketable, :fuid
+  attr_accessible :area_id, :loggable, :name, :photo, :ticketable, :fuid
 
   has_many :logs, :foreign_key => :fuid
   has_many :contacts, :foreign_key => :fuid
